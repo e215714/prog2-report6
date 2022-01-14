0b113a0aeb754e140b1ed6bf39d930b10c93e283
Taisei Maetake <e215703@ie.u-ryukyu.ac.jp>
2022-01-14 22:35:21 +0900

    javaDocの追加

--- app/src/main/java/jp/ac/uryukyu/ie/e215703_e215714/Interact.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215703_e215714/Interact.java
@@ -2,23 +2,42 @@ package jp.ac.uryukyu.ie.e215703_e215714;
 
 import java.util.Scanner;
 
+/**
+ * インタラクティブモードのクラス
+ *  Scanner scanner 標準入力受付用
+ *  MemArray mem 疑似メモリインスタンス
+ *  Interpreter interpreter インタプリタインスタンス
+ * @author Taisei Maetake
+ */
 public class Interact {
     private Scanner scanner;
     private MemArray mem;
     private Interpreter interpreter;
 
+    /**
+     * コンストラクタ
+     * 入力受付用のScannerを用意
+     * MemArrayインスタンスを用意
+     * インタプリタのインスタンスを用意
+     */
     public Interact() {
         scanner = new Scanner(System.in);
         mem = new MemArray();
         interpreter = new Interpreter(mem);
     }
 
+    /**
+     * インタラクティブモードをスタートするメソッド
+     */
     public void start() {
         System.out.println("\u001b[00;36m" + "Brainfuck Interpreter\n" + "\u001b[00m"
-                + "@author Taiga & Taisei\n  $bye - インタラクティブモードを終了\n  $next - メモリをクリアし、ポインタ位置を0に戻す");
+                + "@author e215703 & e215714\n  $bye - インタラクティブモードを終了\n  $next - メモリをクリアし、ポインタ位置を0に戻す");
         interact();
     }
 
+    /**
+     * インタラクト処理をするメソッド
+     */
     public void interact() {
         String line = "";
         String prompt = "\n>>> ";
